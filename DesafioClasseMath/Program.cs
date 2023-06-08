40a293a04fdad427db0b94a6a08986ec87889a1f
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-08 17:08:10 -0300

    Desafio proposto pelo Microsoft Learn com algumas modificações

---
+++ DesafioClasseMath/Program.cs
@@ -0,0 +1,10 @@
+﻿//Implementar um desafio da classe Math que retorna o maior de dois números
+
+Random number = new Random();
+int firstValue = number.Next(101);
+int secondValue = number.Next(101);
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine($"LargerValue: {largerValue}\nFirstValue: {firstValue}\nSecondValue: {secondValue}");
+
+Console.ReadKey();
