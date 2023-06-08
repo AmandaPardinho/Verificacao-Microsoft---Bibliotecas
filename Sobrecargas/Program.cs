59495e5d9be5a57c8d217e44bbcae6751672d644
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-08 14:54:41 -0300

    Projeto para estudar sobrecarga de métodos

---
+++ Sobrecargas/Program.cs
@@ -0,0 +1,11 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(); //retorna valor entre zero e 2147483647 (valor máximo de um int)
+int roll2 = dice.Next(101); //retorna um valor entre zero e 100
+int roll3 = dice.Next(50, 101); //retorna um valor entre 50 e 100
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+Console.ReadKey();
